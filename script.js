4fb62aa271830150fa1a1f7f7f6fb7b640b51336
mukundbondre <msbondre2001@gmail.com>
2023-08-11 22:47:53 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function check() {
+    var pass = document.querySelector("#display").value;
+    if(pass!=123){
+        alert("error")
+    }
+    else{
+        // window.open("https://www.google.com/");
+        window. open("https://www.google.com/", "_self");
+    }
+}
